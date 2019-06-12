e50e3e37ba626d54a64d9597295edf0e23ad0db8
UTKARSH_SHRIVASTAVA <utkarshshrivastava.13@gmail.com>
2019-06-12 18:46:18 +0530

    COMMIT-10_SETUP_Deligating_AimingInfo_to_TankAimingComponent()

--- BATTLE_OF_THE_TANKS/Source/BATTLE_OF_THE_TANKS/Private/Tank.cpp
+++ BATTLE_OF_THE_TANKS/Source/BATTLE_OF_THE_TANKS/Private/Tank.cpp
@@ -7,7 +7,7 @@ ATank::ATank()
 {
  	// Set this pawn to call Tick() every frame.  You can turn this off to improve performance if you don't need it.
 	PrimaryActorTick.bCanEverTick = true;
-	TankAimingComponent = CreateDefaultSubobject<UTankAimingComponent>(FName("Aiming Component"));
+
 }
 
 
@@ -36,6 +36,6 @@ void ATank::SetupPlayerInputComponent(UInputComponent* PlayerInputComponent)
 
 void ATank::AimAT(FVector Hitlocation)
 {
-	TankAimingComponent->AimAt(Hitlocation);
-	
+	auto TankName = GetName();
+	UE_LOG(LogTemp, Warning, TEXT("%s  is Aiming at :	%s"), *TankName,*(Hitlocation.ToString()));
 }
