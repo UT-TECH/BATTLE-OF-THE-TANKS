69b2fb034ae3a6495a338127645591ff500866e9
UTKARSH_SHRIVASTAVA <utkarshshrivastava.13@gmail.com>
2019-06-17 22:22:25 +0530

    COMMIT-20_SETUP_Tank_Track_Class

---
+++ BATTLE_OF_THE_TANKS/Source/BATTLE_OF_THE_TANKS/Private/TankTrack.cpp
@@ -0,0 +1,10 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#include "TankTrack.h"
+
+void UTankTrack::SetTrottle(float Throttle)
+{
+	auto Name = GetName();
+	//UE_LOG(LogTemp, Warning, TEXT("%s Projectile fires %f"),Name,*Throttle);
+
+}
